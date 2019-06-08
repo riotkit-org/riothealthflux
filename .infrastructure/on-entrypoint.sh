93ebcc6563d533e657e25f94420bf91825524087
Free Man <admin@wolnosciowiec.org>
2019-06-08 18:10:36 +0200

    Added migration on each run, as it should be

--- .infrastructure/on-entrypoint.sh
+++ .infrastructure/on-entrypoint.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+echo " >> Upgrading the application"
+sudo -u www-data /bin/bash -c "cd /var/www/html && make migrate"
+
 echo " >> Pre-fetching initial data"
 sudo -u www-data /bin/bash -c "cd /var/www/html && php ./bin/console background-process" &
