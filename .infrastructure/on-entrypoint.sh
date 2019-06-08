11b94d339bb20eb53655a0c14cae901ed334872d
Free Man <admin@wolnosciowiec.org>
2019-06-08 18:16:08 +0200

    Resolved permissions problem

--- .infrastructure/on-entrypoint.sh
+++ .infrastructure/on-entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+echo " >> Preparing permissions"
+touch /var/www/html/var/db/database.sqlite3
+chown www-data:www-data /var/www/html/var/db/ -R
+
 echo " >> Upgrading the application"
 sudo -u www-data /bin/bash -c "cd /var/www/html && make migrate"
 
