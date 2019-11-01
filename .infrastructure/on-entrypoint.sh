a7bf9130797cea4b5c673c176a8a416c13d63bd3
Free Man <admin@wolnosciowiec.org>
2019-11-01 23:35:32 +0100

    Corrected a typo

--- .infrastructure/on-entrypoint.sh
+++ .infrastructure/on-entrypoint.sh
@@ -3,7 +3,7 @@
 echo " >> Preparing permissions"
 touch /var/www/html/var/db/database.sqlite3
 chown www-data:www-data /var/www/html/var/db/ -R
-chmod 755 /var/www/html/var/db/-R
+chmod 755 /var/www/html/var/db/ -R
 
 echo " >> Upgrading the application"
 sudo -u www-data /bin/bash -c "cd /var/www/html && make migrate"
