9f9fe872e3a3f10198280b6729c8d0fd9d1a87c2
Andrew Johnson <blackandred@users.noreply.github.com>
2020-12-02 06:21:04 +0100

    WIP: UptimeAdminBoard -> RiotHealthFlux - wait for containters to get up

--- php.sh
+++ php.sh
@@ -4,4 +4,4 @@ exec docker run --rm --name php_8_0 \
   -e RIOT_PROVIDERS=${RIOT_PROVIDERS} \
   -e INFLUXDB_URL=${INFLUXDB_URL} \
   --network host \
-  -v $(pwd):/$(pwd) --workdir=/$(pwd) php:8.0-rc-alpine "$@"
+  -v $(pwd):/$(pwd) --workdir=/$(pwd) php:8.0-alpine "$@"
--- tests/IntegrationalTestCase.php
+++ tests/IntegrationalTestCase.php
@@ -2,6 +2,8 @@
 
 namespace Tests;
 
+use GuzzleHttp\Client;
+
 class IntegrationalTestCase extends TestCase
 {
     public function setUp(): void
@@ -9,6 +11,9 @@ class IntegrationalTestCase extends TestCase
         parent::setUp();
 
         exec('rkd :test-containers:clear :test-containers:setup');
+
+        $this->waitForAddress('http://localhost:8000');
+        $this->waitForAddress('http://localhost:8086/health');
     }
 
     public function tearDown(): void
@@ -19,5 +24,22 @@ class IntegrationalTestCase extends TestCase
     public static function tearDownAfterClass(): void
     {
         exec('rkd :test-containers:clear > /dev/null');
+    }
+
+    protected function waitForAddress(string $address, int $seconds = 10)
+    {
+        $client = new Client(['http_errors' => false]);
+        $statusCode = null;
+
+        while ($statusCode >= 300 || $statusCode === null) {
+            $statusCode = $client->get($address)->getStatusCode();
+
+            sleep(1);
+            $seconds--;
+
+            if ($seconds === 0) {
+                break;
+            }
+        }
     }
 }
